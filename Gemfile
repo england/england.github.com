4cb3ed8e6f93c82c8f2d3addbecd5138f8ee616f
england <englandpost@gmail.com>
2013-07-14 15:51:01 +0400

    hello foundation

--- Gemfile
+++ Gemfile
@@ -2,7 +2,13 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-tagging'
+gem 'jekyll-assets'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'sass'
+
+gem 'zurb-foundation'
+gem 'compass'
+
 gem 'albino'
 gem 'redcarpet'
-gem 'sass'
-gem 'slim'
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,8 @@
 require 'jekyll/tagging'
+require "jekyll-assets"
+require "jekyll-assets/compass"
+
+require "zurb-foundation"
+
+Sprockets.append_path Compass::Frameworks['foundation'].stylesheets_directory
+Sprockets.append_path Compass::Frameworks['foundation'].stylesheets_directory + '../js'
